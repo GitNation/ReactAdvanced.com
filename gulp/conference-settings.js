c47de5de33d6c8548b73dc365a3a0481e817ee2f
Oleg Proskurin <regx@usul.su>
2020-02-20 12:32:42 +0300

    Connect to content-layer

---
+++ gulp/conference-settings.js
@@ -0,0 +1,18 @@
+const tagColors = {
+  default: {
+    tagBG: 'black',
+    color: 'white',
+  },
+};
+
+const speakerAvatar = {
+  dimensions: {
+    avatarWidth: 500,
+    avatarHeight: 500,
+  },
+};
+
+module.exports = {
+  tagColors,
+  speakerAvatar,
+};
